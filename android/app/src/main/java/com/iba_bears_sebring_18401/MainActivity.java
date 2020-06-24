765e702b7db728977fb8e40b1d63eeb1313e145a
Crowdbotics <team@crowdbotics.com>
2020-06-24 18:22:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/iba_bears_sebring_18401/MainActivity.java
@@ -0,0 +1,15 @@
+package com.iba_bears_sebring_18401;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "iba_bears_sebring_18401";
+    }
+}
